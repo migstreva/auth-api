25bc0c49b647c4d90a9396f92c06a864418fed5b
migstreva <miguelstreva@outlook.com>
2026-03-30 14:35:23 -0300

    chore: initial project setup with Docker, profiles and Flyway

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+)
