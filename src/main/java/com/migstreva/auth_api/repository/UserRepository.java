86b5b077e83abd21f4b02f8cee60d8a8732fbf90
migstreva <miguelstreva@outlook.com>
2026-05-03 20:50:25 -0300

    refactor: Removed findByEmail method from UserRepository

--- src/main/java/com/migstreva/auth_api/repository/UserRepository.java
+++ src/main/java/com/migstreva/auth_api/repository/UserRepository.java
@@ -9,7 +9,6 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByUsername(String username);
-    Optional<User> findByEmail(String email);
     Optional<User> findByUsernameOrEmail(String usernameOrEmail, String usernameOrEmail2);
 
     boolean existsByUsernameOrEmail(String username, String email);
