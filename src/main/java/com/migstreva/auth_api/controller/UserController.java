b957b40549351ea89e7672384c589f9495ff309e
migstreva <miguelstreva@outlook.com>
2026-05-03 21:42:29 -0300

    feat: Made save method from UserController return URI location into ResponseEntity's body.

--- src/main/java/com/migstreva/auth_api/controller/UserController.java
+++ src/main/java/com/migstreva/auth_api/controller/UserController.java
@@ -7,6 +7,7 @@ import com.migstreva.auth_api.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,12 +23,12 @@ public class UserController implements GenericController{
     private final UserMapper mapper;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('ADMIN')")
-    public void save(@RequestBody @Valid UserRegisterDTO dto) {
+    public ResponseEntity<Void> save(@RequestBody @Valid UserRegisterDTO dto) {
         User user = mapper.toEntity(dto);
         service.save(user);
         URI location = generateHeaderLocation(user.getId());
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping
